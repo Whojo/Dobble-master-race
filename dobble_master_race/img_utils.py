12da8236c9d485148272e57f4c795638d486eb28
Pierre Seguin <pierre.seguin@epita.fr>
2022-05-07 21:11:14 +0200

    Added CLI

--- dobble_master_race/img_utils.py
+++ dobble_master_race/img_utils.py
@@ -7,41 +7,48 @@ def _flood_img_border(img, line, mask, start, index):
     seed[index] = np.argmin(line)
 
     while True:
-        cv2.floodFill(img, mask, seed, (0,0,0))
+        cv2.floodFill(img, mask, seed, (0, 0, 0))
         seed[index] = np.argmin(line)
         if seed[index] == 0:
             break
-    
-    
+
+
 def mask_with_inner(img_base: np.array, gray_threshold: int) -> np.array:
     img = img_base.copy()
-    mask = (img.sum(axis=-1) < (gray_threshold * 3))
+    mask = img.sum(axis=-1) < (gray_threshold * 3)
     # Cf floodFill doc
-    pad_mask = np.pad(mask, ((1,1), (1,1))).astype(np.uint8)
-    
+    pad_mask = np.pad(mask, ((1, 1), (1, 1))).astype(np.uint8)
+
     # Start floodFill on every pixel of the image borders
     _flood_img_border(img, pad_mask[1, 1:-1], pad_mask, 0, 0)
     _flood_img_border(img, pad_mask[-2, 1:-1], pad_mask, img.shape[0] - 1, 0)
     _flood_img_border(img, pad_mask[1:-1, 1], pad_mask, 0, 1)
     _flood_img_border(img, pad_mask[1:-1, -2], pad_mask, img.shape[1] - 1, 1)
-    
-    
+
     return ~(pad_mask[1:-1, 1:-1].astype(np.bool8) ^ mask)
 
-def sample_images(images: np.ndarray, masks: np.ndarray, nb_samples: int = 50000) -> np.ndarray:
+
+def sample_images(
+    images: np.ndarray, masks: np.ndarray, nb_samples: int = 50000
+) -> np.ndarray:
     nb_sample_per_image = nb_samples // len(images)
 
-    sample_pxls = images[0][masks[0]][0] # Initialize arbitrary to be able to use np.vstack
+    sample_pxls = images[0][masks[0]][
+        0
+    ]  # Initialize arbitrary to be able to use np.vstack
 
     for img, mask in zip(images[1:], masks[1:]):
         pxls = img[mask]
-        new_sample_pxls_id = np.random.choice(pxls.shape[0], size=nb_sample_per_image, replace=False)
+        new_sample_pxls_id = np.random.choice(
+            pxls.shape[0], size=nb_sample_per_image, replace=False
+        )
         sample_pxls = np.vstack((sample_pxls, pxls[new_sample_pxls_id]))
 
     return sample_pxls
 
+
 def get_hist(color_map: np.ndarray, nb_colors) -> np.ndarray:
     hist = np.bincount(color_map, minlength=nb_colors)
     norm_hist = hist / hist.max()
-    
+
     return norm_hist
---
+++ process.py
@@ -0,0 +1,141 @@
+"""
+Tested with Python 3.8.5
+
+Sample processing tool:
+
+- Takes a directory as input
+- and a path to the output file to produce
+- then classifies each image
+- and produces the correct CSV output
+
+=> Just update the "MyClassifier" methods!
+"""
+
+from argparse import ArgumentParser
+import os
+import os.path
+from typing import Dict, List
+from dobble_master_race.transformers import HuMomentsColorsHistogram
+import cv2
+import numpy as np
+from sklearn.pipeline import make_pipeline
+import pickle
+
+# You may use the following imports. Please warn me if you need something else.
+# import cv2
+# import skimage
+
+
+class MyClassifier:
+    """Sample classifier class to update."""
+
+    def __init__(self) -> None:
+        curr_dir = os.path.dirname(os.path.abspath(__file__))
+        models_dir = os.path.join(curr_dir, "dobble_master_race/models_save")
+        kmeans_path = os.path.join(models_dir, "kmeans_save.sav")
+        scaler_path = os.path.join(models_dir, "scaler_save.sav")
+        classifier_path = os.path.join(models_dir, "classifier_save.sav")
+
+        hist_args = {"saved_kmeans": kmeans_path}
+        transformer = HuMomentsColorsHistogram(6, 10, hist_args, {})
+        with open(scaler_path, "rb") as scaler_file:
+            scaler = pickle.load(scaler_file)
+        with open(classifier_path, "rb") as classifier_file:
+            classifier = pickle.load(classifier_file)
+
+        self.pipeline = make_pipeline(transformer, scaler, classifier)
+
+    def classify_image(self, image_path: str) -> int:
+        """Classify the file for which the path is given,
+        returning the correct class as output (1-56) or -1 to reject
+
+        Args:
+            image_path (str): Full path to the image to process
+
+        Returns:
+            int: Class of the symbol contained (1-56 or -1).
+
+        WARNING: `0` is not a valid class here.
+                 You may need to adjust your classifier outputs (typically 0-55).
+        """
+        img = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+        cls_id = self.pipeline.predict(np.array([img]))[0]
+        return cls_id
+
+
+def save_classifications(image_classes: Dict[str, int], output_path: str):
+    """Save classification results to a CSV file
+
+    Args:
+        image_classes (Dict[str,int]): dict of base filename -> class id
+        output_path (str): will store a CSV in the following format:
+    ```csv
+    filename,class_id
+    filename,class_id
+    (no header, accept any leading 0 for class_id)
+    ```
+    """
+    with open(output_path, "w", encoding="utf8") as out_file:
+        for filename, cls_id in image_classes.items():
+            out_file.write(f"{filename},{cls_id:02d}\n")
+
+
+def find_png_files_in_dir(input_dir_path: str) -> List[str]:
+    """Returns a list of PNG files contained in a directory, without any leading directory.
+
+    Args:
+        input_dir_path (str): Path to directory
+
+    Returns:
+        List[str]: List of PNG files (no leading directory).
+    """
+    with os.scandir(input_dir_path) as entry_iter:
+        result = [
+            entry.name
+            for entry in entry_iter
+            if entry.name.endswith(".png") and entry.is_file()
+        ]
+        return result
+
+
+def main():
+    """Main function."""
+    # CLI
+    parser = ArgumentParser(description="Sample processing program.")
+    parser.add_argument(
+        "--test_dir",
+        required=True,
+        help="Path to the directory containing the test files.",
+    )
+    parser.add_argument("--output", required=True, help="Path to output CSV file.")
+    args = parser.parse_args()
+
+    # Find files
+    files = find_png_files_in_dir(args.test_dir)
+
+    # Create classifier
+    clf = MyClassifier()
+
+    # Let's go
+    results = {}
+    print("Processing files...")
+    for file in files:
+        print(file)
+        file_full_path = os.path.join(args.test_dir, file)
+        cls_id = -1
+        try:
+            cls_id = clf.classify_image(file_full_path)
+            print(f"\t-> {cls_id}")
+        except:
+            print(f"Warning: failed to process file '{file_full_path}'.")
+            raise
+        results[file] = cls_id
+    print("Done processing files.")
+
+    # Save predictions
+    save_classifications(results, args.output)
+    print(f"Predictions saved in '{args.output}'.")
+
+
+if __name__ == "__main__":
+    main()
